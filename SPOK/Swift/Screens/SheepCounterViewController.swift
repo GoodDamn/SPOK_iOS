5b0e74dcb155c00d82f8473164ecb564f0c9d4f1
GoodDamn <grigorydum@gmail.com>
2024-01-16 06:43:07 -0800

    Sheep counter gameplay

--- SPOK/Swift/Screens/SheepCounterViewController.swift
+++ SPOK/Swift/Screens/SheepCounterViewController.swift
@@ -15,6 +15,8 @@ class SheepCounterViewController
     
     private var mExtrabold: UIFont? = nil
     
+    private var mNextSheep: Sheep? = nil
+    
     private weak var mPrevc: UITextViewPhrase? = nil
     
     override func viewDidLoad() {
@@ -41,20 +43,7 @@ class SheepCounterViewController
                 sizeSquare: 0.084
             )
         
-        let sizeSheep = w * 0.25
         
-        let sheep = UIImageView(
-            frame: CGRect(
-                x: (w - sizeSheep) * 0.5,
-                y: (h - sizeSheep) * 0.5,
-                width: sizeSheep,
-                height: sizeSheep
-            )
-        )
-        
-        sheep.image = UIImage(
-            named: "sheep"
-        )
         
         let ofx = w * 0.217
         let lTap = UILabel(
@@ -79,8 +68,10 @@ class SheepCounterViewController
         lTap.frame.origin.x =
             (w-lTap.frame.width) * 0.5
         
+        mNextSheep = createSheep()
+        
         view.addSubview(btnClose)
-        view.addSubview(sheep)
+        view.addSubview(mNextSheep!)
         view.addSubview(lTap)
         
         let g = UITapGestureRecognizer(
@@ -120,6 +111,22 @@ class SheepCounterViewController
         
         let f = view.frame
         
+        let sheep = createSheep()
+        
+        sheep.jump(
+            onX: -f.width
+        )
+        
+        view.addSubview(sheep)
+        
+        mNextSheep?.jumpAnim(
+            onX: f.width / 2
+        )
+        
+        sheep.landAnim()
+        
+        mNextSheep = sheep
+        
         let a = UITextViewPhrase(
             frame: CGRect(
                 x: 0,
@@ -141,6 +148,95 @@ class SheepCounterViewController
         mPrevc = a
         
         mCounter += 1
+    }
+    
+    private func createSheep() -> Sheep {
+        
+        let f = view.frame
+        
+        let sizeSheep = f.width * 0.25
+        
+        let sheep = Sheep(
+            frame: CGRect(
+                x: (f.width - sizeSheep) * 0.5,
+                y: (f.height - sizeSheep) * 0.5,
+                width: sizeSheep,
+                height: sizeSheep
+            )
+        )
+        
+        sheep.image = UIImage(
+            named: "sheep"
+        )
+        
+        return sheep
+    }
+    
+}
+
+
+class Sheep
+    : UIImageView {
+    
+    private var mAmp: CGFloat = 0
+    private var mAngle: CGFloat = 0
+    
+    deinit {
+        print("Sheep: deinit()")
+    }
+    
+    override init(
+        frame: CGRect
+    ) {
+        mAmp = frame.height * -1.5
+        mAngle = -25/180 * .pi
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func jump(
+        onX: CGFloat
+    ) {
+        transform = CGAffineTransform(
+            translationX: onX + frame.width,
+            y: mAmp
+        ).rotated(
+            by: mAngle
+        )
+    }
+    
+    func land() {
+        transform = CGAffineTransform(
+            rotationAngle: 0
+        ).translatedBy(
+            x: 0,
+            y: 0
+        )
+        
+    }
+    
+    func jumpAnim(
+        onX: CGFloat
+    ) {
+        UIView.animate(
+            withDuration: 0.8,
+            animations: {
+                self.jump(onX: onX)
+            }
+        ) { b in
+            self.removeFromSuperview()
+        }
+    }
+    
+    func landAnim() {
+        UIView.animate(
+            withDuration: 0.8
+        ) {
+            self.land()
+        }
     }
     
 }
