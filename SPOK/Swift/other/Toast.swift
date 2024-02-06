bf7dd716088c4235c740984d4e371f759fe07cb6
GoodDamn <grigorydum@gmail.com>
2024-02-06 06:52:22 -0800

    Toast Refactoring

--- SPOK/Swift/other/Toast.swift
+++ SPOK/Swift/other/Toast.swift
@@ -8,9 +8,8 @@
 import Foundation;
 import UIKit;
 
-class Toast{
+class Toast {
     
-    private var mIsShowing: Bool
     private var mText: String
     private var mDuration: Double
     
@@ -20,61 +19,26 @@ class Toast{
     ) {
         mText = text;
         mDuration = duration;
-        mIsShowing = false;
     }
     
     public func show() {
-        if (mIsShowing) {
-            return;
-        }
         
-        mIsShowing = true
-        
-        guard let window = UIApplication
-            .shared
-            .connectedScenes
-            .compactMap({($0 as? UIWindowScene)?.windows ?? []}).last?[0] else {
-            return;
-        };
-        
-        let label = UILabel();
-        label.text = mText;
-        label.textColor = .white;
-        label.font= UIFont.systemFont(
-            ofSize: 18
+        let c = UIAlertController(
+            title: mText,
+            message: nil,
+            preferredStyle: .alert
         )
         
-        label.backgroundColor = .white
-        label.numberOfLines = 0
-        label.textAlignment = .center
-    
-        let textSize = label.intrinsicContentSize;
-        let lwidth=min(textSize.width, window.frame.width-40);
-        let lheight = (textSize.height/window.frame.height)*30;
-    
-        label.frame = CGRect(x: 20, y: window.frame.height, width: lwidth+30, height: max(lheight, textSize.height+20));
-        label.center.x=window.center.x;
-        label.layer.cornerRadius=15;
-        label.layer.shadowOffset = CGSize(width: 0, height: 0);
-        label.layer.shadowOpacity=1.0;
-        label.layer.shadowRadius=8;
-        label.layer.shouldRasterize=true;
-        label.layer.rasterizationScale=UIScreen.main.scale;
-        label.layer.masksToBounds=true;
-        window.addSubview(label);
+        c.addAction(UIAlertAction(
+            title: "OK",
+            style: .default)
+        )
         
-        UIView.animate(withDuration: 0.25, animations: {
-            label.center.y = window.frame.height-90;
-        }){ _ in ()
-            DispatchQueue.main.asyncAfter(deadline: .now()+self.duration){
-                UIView.animate(withDuration:            0.25, animations:{
-                    label.alpha=0;
-                    label.center.y = window.frame.height;
-                    }){ _ in ()
-                    label.removeFromSuperview();
-                    self.isShowing = false;
-                };
-            }
-        };
+        Utils.main()
+            .present(
+                c,
+                animated: true
+            )
+        
     }
 }
